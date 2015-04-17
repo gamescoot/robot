0f110be7bc8f83be8edb55a9139c5ced30e9cb03
Sam-Browder <browdess@rose-hulman.edu>
2015-04-16 20:14:34 -0400

    AI
    enemy now walks back and forth and respawns

--- Robot Number II/Assets/Scripts/EnemyController.cs
+++ Robot Number II/Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class EnemyController : MonoBehaviour {
-
+	
 	//floats
 	public float maxSpeed = 3;
 	public float speed = 50f;
@@ -13,7 +13,7 @@ public class EnemyController : MonoBehaviour {
 	public bool canDoubleJump;
 	
 	public int direction = 1;
-
+	
 	//refrences
 	private Rigidbody2D rb2d;
 	private Animator anim;
@@ -21,43 +21,80 @@ public class EnemyController : MonoBehaviour {
 	//Game values
 	private float health =100;
 	
-	
+	private float time = 0.0f;
 	// Use this for initialization
 	void Start () {
 		
 		rb2d = gameObject.GetComponent<Rigidbody2D> ();
-		//anim = gameObject.GetComponent<Animator> ();
-
-		//Debug.Log ("hello world");
+		anim = gameObject.GetComponent<Animator> ();
 	}
 	
-	// Update is called once per frame
-	//public Transform testProj;
+
 	void Update () {
 
-		//Debug.Log(health);
+		anim.SetBool("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs(rb2d.velocity.x));
 
+		time += Time.deltaTime;
+		
 		if (health <= 0.0) {
+			GameObject newEnemy = (GameObject) Instantiate(Resources.Load("Enemy"));
+
+			newEnemy.transform.position = new Vector3(4.2f,2.2f,0.0f);
 			Destroy (gameObject);
 		}
-		
-		//anim.SetBool("Grounded", grounded);
-		//anim.SetFloat ("Speed", Mathf.Abs(rb2d.velocity.x));
-		
 
+
+
+		if(time > 2)
+		{
+			time= time -2;
+			this.direction = this.direction*-1;
+		}
+
+		if (direction < -0.1f) {
+			direction = -1;
+			transform.localScale = new Vector3(-1,1,1);
+		}
+		if (direction > 0.1f) {
+			transform.localScale = new Vector3(1,1,1);
+			direction = 1;
+		}
+		
 	}
 	
 	// do all physics in here
 	void FixedUpdate()
 	{
-	
+		Vector3 easeVelocity = rb2d.velocity;
+		easeVelocity.y = rb2d.velocity.y;
+		easeVelocity.z = 0.0f;
+		easeVelocity.x *= 0.80f;
+		
+		//fake friction
+		if (grounded) {
+			rb2d.velocity = easeVelocity;
+		}
+		
+		
+		//moves player
+		float h = direction;
+		rb2d.AddForce ((Vector2.right * speed) * h);
+		
+		
+		//limits the speed
+		if (rb2d.velocity.x > maxSpeed) {
+			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
+		}
+		
+		if (rb2d.velocity.x < -maxSpeed) {
+			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
+		}
 	}
-
+	
 	void ApplyDamage(float damage){
 		this.health = this.health - damage;
 	}
 	
 	
 }
-
-
--- Robot Number II/Assets/Scripts/GroundCheck.cs
+++ Robot Number II/Assets/Scripts/GroundCheck.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 public class GroundCheck : MonoBehaviour {
 
-	private Player player;
+	private Player parent;
 
 	void Start()
 	{
-		player = gameObject.GetComponentInParent<Player> ();
+		parent = gameObject.GetComponentInParent<Player> ();
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		player.grounded = true;
+		parent.grounded = true;
 	}
 
 	void OnTriggerStay2D(Collider2D col)
 	{
-		player.grounded = true;
+		parent.grounded = true;
 	}
 
 	void OnTriggerExit2D(Collider2D col)
 	{
-		player.grounded = false;
+		parent.grounded = false;
 	}
 }
