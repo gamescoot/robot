adb0c33900359b2a1a6f592f5ff73916eac7beef
Sam-Browder <browdess@rose-hulman.edu>
2015-04-17 16:45:31 -0400

    updated prfabs

--- Robot Number II/Assets/Scripts/GroundCheck.cs
+++ Robot Number II/Assets/Scripts/GroundCheck.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 public class GroundCheck : MonoBehaviour {
 
-	private Player parent;
+	private ICharacter parent;
 
 	void Start()
 	{
-		parent = gameObject.GetComponentInParent<Player> ();
+		parent = gameObject.GetComponentInParent<ICharacter> ();
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		parent.grounded = true;
+		parent.SetGrounded(true);
 	}
 
 	void OnTriggerStay2D(Collider2D col)
 	{
-		parent.grounded = true;
+		parent.SetGrounded(true);
 	}
 
 	void OnTriggerExit2D(Collider2D col)
 	{
-		parent.grounded = false;
+		parent.SetGrounded(false);
 	}
 }
