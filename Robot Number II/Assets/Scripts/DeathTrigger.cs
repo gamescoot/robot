87050b771cd00cc1c9782d9961f516be371c058d
unknown <harrissa@HARRISSA-W20.rose-hulman.edu>
2015-04-21 19:58:00 -0400

    After using all the water in the great lakes and some reserve water from the niagra falls, I may have put out the fire. Please test asap and let me know

---
+++ Robot Number II/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			//Application.LoadLevel(Application.loadedLevel);
+			other.SendMessage("Die");
+		}
+
+		if (other.gameObject.CompareTag ("Enemy")) {
+			other.SendMessage("Die");
+			//Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Robot Number II/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	private ICharacter parent;
+
+	void Start()
+	{
+		parent = gameObject.GetComponentInParent<ICharacter> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Ground" || col.tag == "Player" || col.tag =="Enemy") {
+			parent.SetGrounded (true);
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.tag == "Ground" || col.tag == "Player"|| col.tag =="Enemy") {
+			parent.SetGrounded (true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.tag == "Ground" || col.tag == "Player"|| col.tag =="Enemy") {
+			parent.SetGrounded (false);
+		}
+	}
+}
